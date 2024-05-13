91104ae54ad6dc56732639b3e2dca968421007b5
pablovela5620 <pablovela5620@gmail.com>
2024-05-13 11:21:45 -0500

    ```text chore: Update mini-dust3r dependency to version 0.1.1 in pixi.lock
    This commit updates the mini-dust3r dependency in the pixi.lock file from version 0.1.0 to version 0.1.1. The new version includes bug fixes and performance improvements, and a single image + multi image gradio demo

--- tools/app.py
+++ tools/app.py
@@ -1,11 +1,12 @@
 import gradio as gr
+
+# import spaces
 import torch
 from gradio_rerun import Rerun
 import rerun as rr
 import rerun.blueprint as rrb
 from pathlib import Path
 import uuid
-
 
 from mini_dust3r.api import OptimizedResult, inferece_dust3r, log_optimized_result
 from mini_dust3r.model import AsymmetricCroCo3DStereo
@@ -16,11 +17,21 @@ model = AsymmetricCroCo3DStereo.from_pretrained(
 ).to(DEVICE)
 
 
-def predict(image_name_list: list[str]):
+# @spaces.GPU
+def predict(image_name_list: list[str] | str):
+    # check if is list or string and if not raise error
+    if not isinstance(image_name_list, list) and not isinstance(image_name_list, str):
+        raise gr.Error(
+            f"Input must be a list of strings or a string, got: {type(image_name_list)}"
+        )
     uuid_str = str(uuid.uuid4())
     filename = Path(f"/tmp/gradio/{uuid_str}.rrd")
     rr.init(f"{uuid_str}")
     log_path = Path("world")
+
+    if isinstance(image_name_list, str):
+        image_name_list = [image_name_list]
+
     optimized_results: OptimizedResult = inferece_dust3r(
         image_dir_or_list=image_name_list,
         model=model,
@@ -40,11 +51,22 @@ with gr.Blocks(
 ) as demo:
     # scene state is save so that you can change conf_thr, cam_size... without rerunning the inference
     gr.HTML('<h2 style="text-align: center;">Mini-DUSt3R Demo</h2>')
-    with gr.Column():
-        inputfiles = gr.File(file_count="multiple")
-        rerun_viewer = Rerun(height=900)
+    with gr.Tab(label="Single Image"):
+        with gr.Column():
+            singe_image = gr.Image(type="filepath")
+            run_btn_single = gr.Button("Run")
+            rerun_viewer_single = Rerun(height=900)
+            run_btn_single.click(
+                fn=predict, inputs=[singe_image], outputs=[rerun_viewer_single]
+            )
+    with gr.Tab(label="Multi Image"):
+        with gr.Column():
+            multi_files = gr.File(file_count="multiple")
+            run_btn_multi = gr.Button("Run")
+            rerun_viewer_multi = Rerun(height=900)
+            run_btn_multi.click(
+                fn=predict, inputs=[multi_files], outputs=[rerun_viewer_multi]
+            )
 
-        run_btn = gr.Button("Run")
-        run_btn.click(fn=predict, inputs=[inputfiles], outputs=[rerun_viewer])
 
 demo.launch()
