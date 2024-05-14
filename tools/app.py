9bf70e98d3ab7676892ff9ed678445b21c774cd3
pablovela5620 <pablovela5620@gmail.com>
2024-05-14 07:43:11 -0500

    refactor: Update create_blueprint function to handle multiple image directories
    This commit refactors the create_blueprint function in app.py and rerun_demo.py to handle multiple image directories. The function now takes a list of image directories as input and generates a blueprint accordingly. This change improves the flexibility of the function and allows for better visualization of 2D and 3D views when there are more than 4 images.

--- tools/app.py
+++ tools/app.py
@@ -17,6 +17,39 @@ model = AsymmetricCroCo3DStereo.from_pretrained(
 ).to(DEVICE)
 
 
+def create_blueprint(image_name_list: list[str], log_path: Path) -> rrb.Blueprint:
+    # dont show 2d views if there are more than 4 images as to not clutter the view
+    if len(image_name_list) > 4:
+        blueprint = rrb.Blueprint(
+            rrb.Horizontal(
+                rrb.Spatial3DView(origin=f"{log_path}"),
+            ),
+            collapse_panels=True,
+        )
+    else:
+        blueprint = rrb.Blueprint(
+            rrb.Horizontal(
+                contents=[
+                    rrb.Spatial3DView(origin=f"{log_path}"),
+                    rrb.Vertical(
+                        contents=[
+                            rrb.Spatial2DView(
+                                origin=f"{log_path}/camera_{i}/pinhole/",
+                                contents=[
+                                    "+ $origin/**",
+                                ],
+                            )
+                            for i in range(len(image_name_list))
+                        ]
+                    ),
+                ],
+                column_shares=[3, 1],
+            ),
+            collapse_panels=True,
+        )
+    return blueprint
+
+
 # @spaces.GPU
 def predict(image_name_list: list[str] | str):
     # check if is list or string and if not raise error
@@ -38,9 +71,12 @@ def predict(image_name_list: list[str] | str):
         device=DEVICE,
         batch_size=1,
     )
+
+    blueprint: rrb.Blueprint = create_blueprint(image_name_list, log_path)
+    rr.send_blueprint(blueprint)
+
     rr.set_time_sequence("sequence", 0)
     log_optimized_result(optimized_results, log_path)
-    # blueprint = rrb.Spatial3DView(origin="cube")
     rr.save(filename.as_posix())
     return filename.as_posix()
 
@@ -51,13 +87,30 @@ with gr.Blocks(
 ) as demo:
     # scene state is save so that you can change conf_thr, cam_size... without rerunning the inference
     gr.HTML('<h2 style="text-align: center;">Mini-DUSt3R Demo</h2>')
+    gr.HTML(
+        '<p style="text-align: center;">Unofficial DUSt3R demo using the mini-dust3r pip package</p>'
+    )
+    gr.HTML(
+        '<p style="text-align: center;">More info <a href="https://github.com/pablovela5620/mini-dust3r">here</a></p>'
+    )
     with gr.Tab(label="Single Image"):
         with gr.Column():
-            singe_image = gr.Image(type="filepath")
+            single_image = gr.Image(type="filepath", height=300)
             run_btn_single = gr.Button("Run")
             rerun_viewer_single = Rerun(height=900)
             run_btn_single.click(
-                fn=predict, inputs=[singe_image], outputs=[rerun_viewer_single]
+                fn=predict, inputs=[single_image], outputs=[rerun_viewer_single]
+            )
+
+            example_single_dir = Path("examples/single_image")
+            example_single_files = sorted(example_single_dir.glob("*.png"))
+
+            examples_single = gr.Examples(
+                examples=example_single_files,
+                inputs=[single_image],
+                outputs=[rerun_viewer_single],
+                fn=predict,
+                cache_examples="lazy",
             )
     with gr.Tab(label="Multi Image"):
         with gr.Column():
--- tools/rerun_demo.py
+++ tools/rerun_demo.py
@@ -1,10 +1,44 @@
 import rerun as rr
+import rerun.blueprint as rrb
 from pathlib import Path
 from argparse import ArgumentParser
 import torch
 
 from mini_dust3r.api import OptimizedResult, inferece_dust3r, log_optimized_result
 from mini_dust3r.model import AsymmetricCroCo3DStereo
+
+
+def create_blueprint(image_name_list: list[str], log_path: Path) -> rrb.Blueprint:
+    # dont show 2d views if there are more than 4 images as to not clutter the view
+    if len(image_name_list) > 4:
+        blueprint = rrb.Blueprint(
+            rrb.Horizontal(
+                rrb.Spatial3DView(origin=f"{log_path}"),
+            ),
+            collapse_panels=True,
+        )
+    else:
+        blueprint = rrb.Blueprint(
+            rrb.Horizontal(
+                contents=[
+                    rrb.Spatial3DView(origin=f"{log_path}"),
+                    rrb.Vertical(
+                        contents=[
+                            rrb.Spatial2DView(
+                                origin=f"{log_path}/camera_{i}/pinhole/",
+                                contents=[
+                                    "+ $origin/**",
+                                ],
+                            )
+                            for i in range(len(image_name_list))
+                        ]
+                    ),
+                ],
+                column_shares=[3, 1],
+            ),
+            collapse_panels=True,
+        )
+    return blueprint
 
 
 def main(image_dir: Path):
@@ -25,6 +59,8 @@ def main(image_dir: Path):
         device=device,
         batch_size=1,
     )
+    blueprint = create_blueprint(image_dir, "world")
+    rr.send_blueprint(blueprint)
     log_optimized_result(optimized_results, Path("world"))
 
 
